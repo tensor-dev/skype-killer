e0b52bd4ae1f99edd250ef0d22a814beec429f04
KornilovVasilii <razz96@mail.ru>
2016-11-19 14:54:57 +0300

    Update Recognizer.js
    debug

--- Recognizer.js
+++ Recognizer.js
@@ -7,28 +7,42 @@ define('Recognizer', function() {
      this.recognition.interimResults = false;
      this.recognition.maxAlternatives = 1;
      this.isStarted = false;
+     this.id = 0;
+     this.onResult = null;
    }
 
-   Recognizer.prototype.startrecog = function(foo){
+   Recognizer.prototype.start = function(){
+     var self = this;
      this.recognition.start();
          this.recognition.onend = function() {
             if(this.isStarted == false){
-               startrecog(foo);
+               this.start();
                this.stop();
             }
             else
             {this.start();}
          }
      this.recognition.onresult = function(event) {
-       var text = event.results[0][0].transcript;
-       foo(text);
+       if (self.onResult) {
+          self.onResult({
+              text: event.results[0][0].transcript,
+              locale: self.locale,
+              id: self.id++
+          })
+      }
      }
    }
 
-   Recognizer.prototype.stoprecog = function(foo){
+   Recognizer.prototype.stop = function(){
+     var self = this;
      this.recognition.onresult = function(event) {
-       var text = event.results[0][0].transcript;
-       foo(text);
+       if (self.onResult) {
+          self.onResult({
+              text: event.results[0][0].transcript,
+              locale: self.locale,
+              id: self.id++
+          })
+      }
      }
      this.isStarted = true;
        this.recognition.onend = function()
