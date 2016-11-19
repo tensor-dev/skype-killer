f9eaa33393457c73807a2f72ca2d8ddf555cca0f
UCS-ZAMENA2\student <everlast.isi@gmail.com>
2016-11-19 16:16:42 +0300

    add stop

--- languageDefinition.js
+++ languageDefinition.js
@@ -1,49 +1,51 @@
 define('LocaleDetectorModule', function() {
+	
 
-   function LocaleDetector() {
-   }
+	LocaleDetector.onResult = '';
 
-   LocaleDetector.prototype.detect = function() {
-      var streamer = new ya.speechkit.SpeechRecognition();
-      var localeDetect = this;
-      streamer.start({
-         apikey: 'e849ef32-1ef3-4085-83cb-9e0f580689df',
-         initCallback: function() {
-            console.log("Началась запись звука.");
-         },
-         biometry: 'gender,language,group',
-         dataCallback: function(text, done, merge, words, biometry) {
+	function LocaleDetector() {}
 
-            if (done) {
-               var locale = '';
-               var value = 0;
-               $.each(biometry, function(j, bio) {
-                  if (bio.confidence > value && bio.tag == 'language') {
-                     value = bio.confidence;
-                     locale = bio.class;
-                  }
-               });
-               if (localeDetect.onResult) {
-                  localeDetect.onResult(locale);
-               }
-               streamer.stop();
+	LocaleDetector.prototype.detect = function() {
+		var streamer = new ya.speechkit.SpeechRecognition();
+		var localeDetect = this;
+	    streamer.start({
+		  apikey: 'e849ef32-1ef3-4085-83cb-9e0f580689df',
+		  initCallback: function () {
+		    console.log("Началась запись звука.");
+		  },
+		  biometry: 'gender,language,group',
+		  dataCallback: function (text, done, merge, words, biometry) {
 
-            }
-         },
-         errorCallback: function(err) {
-            console.log("Возникла ошибка: " + err);
-         },
-         // Содержит сведения о ходе процесса распознавания.
-         infoCallback: function(sent_bytes, sent_packages, processed, format) {
-         },
-         // Будет вызвана после остановки распознавания.
-         stopCallback: function() {
-            console.log("Запись звука прекращена.");
-         },
-         particialResults: true,
-         utteranceSilence: 60
-      });
-   };
+		  		if(done) {
+		  			var locale = '';
+				    var value = 0;
+				    $.each(biometry, function (j, bio) {
+				        if(bio.confidence > value && bio.tag == 'language') {
+				    		value = bio.confidence;
+				    		locale = bio.class;
+				    	}
+				    });
+				    if(localeDetect.onResult) {
+				    	localeDetect.onResult(locale);
+				    }
+				    streamer.stop();
+				    
+			  	}
+		  },
+		  errorCallback: function (err) {
+		    console.log("Возникла ошибка: " + err);
+		  },
+		  // Содержит сведения о ходе процесса распознавания.
+		  infoCallback: function (sent_bytes, sent_packages, processed, format) {},
+		  // Будет вызвана после остановки распознавания.
+		  stopCallback: function () {
+		    console.log("Запись звука прекращена.");
+		  },
+		  particialResults: true,
+		  utteranceSilence: 60
+		 });
+		}
 
-   return LocaleDetector;
+
+	return LocaleDetector;
 });
